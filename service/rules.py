eb173229218d7f191255af2f2e20ce6cdc734673
Alex Acosta <alexcytek@gmail.com>
2021-06-20 22:44:05 -0700

    Simplifying unique records comparison

--- service/rules.py
+++ service/rules.py
@@ -1,7 +1,3 @@
-import hashlib
-import json
-
-
 def unique_residences(record):
     """ unique_residences method
 
@@ -20,13 +16,10 @@ def unique_residences(record):
 
     """
     unique_records = []
-    residences_tokens = []
     for report in record['reports']:
         if report['title'] == 'Residences Report':
             for residence in report['residences']:
-                residence_tokenized = hashlib.md5(json.dumps(residence).encode("utf-8")).hexdigest()
-                if residence_tokenized not in residences_tokens:
-                    residences_tokens.append(residence_tokenized)
+                if residence not in unique_records:
                     unique_records.append(residence)
             report['residences'] = unique_records
 
